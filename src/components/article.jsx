6c48a2c7543b063922677fad225893d83b5fd681
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-05-01 19:27:59 +0300

    feat: add article && article list jsx files

---
+++ src/components/article.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Article() {
+  return (
+    <article>
+        <h3 className='title'></h3>
+        <small className='date'></small>
+        <p className='preview'></p>
+    </article>
+  )
+}
