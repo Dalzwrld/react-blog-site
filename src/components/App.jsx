62b7b3230e4a662528763136e43dca1fa05de7d1
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-05-01 19:21:09 +0300

    feat: add jsx files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import blogData from "../data/blog";
 import Header from "./components/header";
 import About from "./components/about";
-import Article from "./components/article";
+import articleList from "./components/article-list";
 
 console.log(blogData);
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <About />
+      <Article />
     </div>
   );
 }
