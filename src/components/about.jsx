377cfbd64963ef9ed084cb3559478394ba1e9969
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-05-01 19:19:19 +0300

    feat: add about jsx file

---
+++ src/components/about.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function About() {
+  return (
+    <aside>
+        <img src="{image}" alt="blog logo" />
+        <p className='about'>
+
+        </p>
+    </aside>
+  )
+}
+
+export default About
